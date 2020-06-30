8fb644d2247dda28ff014fad99c03e5001d9bbec
edensongjbs <edensongjbs@gmail.com>
2020-06-30 14:12:31 -0400

    built create route for membership class

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,2 +1,13 @@
 class MembershipsController < ApplicationController
+    
+    def create
+        @membership=Membership.create(membership_params)
+    end
+
+    private
+
+    def membership_params
+        params.require(:membership).permit(:group_id, :user_id)
+    end
+
 end
