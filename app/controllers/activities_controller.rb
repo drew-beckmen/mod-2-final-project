426bda768957192524c60c4c97c8e8d47dc95424
edensongjbs <edensongjbs@gmail.com>
2020-06-30 10:27:20 -0400

    built basic crud for activities

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,52 @@
 class ActivitiesController < ApplicationController
+    
+    before_action :current_activity, only: [:edit, :update, :show, :destroy]
+
+    def index
+        @user = current_user
+        @activities = Activity.select { |activity| activity.goal.user == current_user }
+    end
+    
+    def new
+        @activity = Activity.new
+        @user = current_user
+        @activity_types = ActivityType.all
+        @goals = current_user.goals
+    end
+
+    def create
+        @activity = Activity.create(activity_params)
+        redirect_to activity_path(@activity)
+    end
+
+    def edit
+        @user = current_user
+        @activity_types = ActivityType.all
+        @goals = current_user.goals
+    end
+
+    def update
+        @activity.update(activity_params)
+        redirect_to activity_path(@activity)
+    end
+
+    def destroy
+        @activity.destroy
+        redirect_to activities_path
+    end
+
+    private
+
+    def activity_params
+        params.require(:activity).permit(:activity_type_id, :description, :goal_id, :activity_date, :duration, :rating)
+    end
+
+    def current_activity
+        @activity = Activity.find(params[:id])
+    end
+
+    def current_user
+        # session[:user_id]
+        User.first ## CHANGE!!!
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   resources :activity_types, only: [:index, :create]
 
   # Goals and Goal Activities will have all RESTful routes 
-  resources :activites 
+  resources :activities 
   resources :goals
   resources :users, only: [:create, :new, :edit, :show, :update, :destroy]
 end
